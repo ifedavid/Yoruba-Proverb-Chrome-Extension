b3a676c95f6c2a0cb0c5203d10204d09f0e5483a
ifeoluwa Adeoye <onigbinde.i@gmail.com>
2019-08-20 16:06:09 +0100

    Added some styling

--- tabpage.js
+++ tabpage.js
@@ -1,5 +1,5 @@
 console.log(data);
 	var random = Math.floor(Math.random() * 48);
-	 document.getElementById('proverb').innerHTML = data[random].proverb;
+	 document.getElementById('proverb').innerHTML = "\"" + data[random].proverb + "\"";
 	 document.getElementById('translation').innerHTML ="Translation: " + data[random].translation;
 	 document.getElementById('meaning').innerHTML = "Meaning: " + data[random].meaning;
